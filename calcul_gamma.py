84fbf20e768b946dbade0c6518fc6237d829a0b4
josephdeteix <47495683+josephdeteix@users.noreply.github.com>
2022-05-09 11:20:17 +0200

    Update calcul_gamma.py

--- calcul_gamma.py
+++ calcul_gamma.py
@@ -19,19 +19,26 @@ def deux_tablo_plot(name_file):
     data = txt_en_numpy(name_file)
     t_max = data.shape[0]
     temps = np.linspace(0,t_max,t_max)
-    plt.plot(temps,data,label = 'avec la balance', color = 'r')
+    #plt.plot(temps,data,label = 'Forces', color = 'r')
+    #plt.title('Force en fonction du temps')
+    #plt.xlabel('Temps sans unité')
+    #plt.ylabel('Masse en grammes')
     return temps,data
 
 def donne_gamma(data):
+    masse = data[-1]
     mmax = np.max(data)
-    fmax = 9.81*mmax*(10**(-3))
+    fmax = 9.81*(mmax - masse)*(10**(-3))
     gamma = fmax/(4*3.141592*r)
     return gamma
 
 if __name__ == "__main__" :
-    temps, data = deux_tablo_plot('eau_4')
-    #print(np.mean(data))
-    gamma = donne_gamma(data)
-    plt.legend()
-    print(gamma)
+    Gamma = np.zeros(4)
+    for i in range(1,5):
+    	temps, data = deux_tablo_plot('resultat_txt/eau_{}'.format(i))
+    	#print(np.mean(data))
+    	gamma = donne_gamma(data)
+    	Gamma[i-1] = gamma
+    #plt.legend()
+    print(np.mean(Gamma), np.std(Gamma))
     
